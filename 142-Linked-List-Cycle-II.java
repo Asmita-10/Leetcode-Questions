cb68d9830dfe13825e769f857f1e14312ec89399
Asmita Tiwari <asmitatiwari2007@gmail.com>
2026-08-01 07:59:58 +0530

    [142] [Time Beats: 100.00%] [Memory Beats: 50.52%] - LeetPush

--- 142-Linked-List-Cycle-II.java
+++ 142-Linked-List-Cycle-II.java
@@ -21,7 +21,7 @@ public class Solution {
             while(slow!=fast){
                 slow=slow.next;
             fast=fast.next;}
-            return slow;
+            return fast;
             }
         }
         return null;
